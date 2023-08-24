740957edc9cfc74c8453674349903945c1ef29ef
Dharshinipa <126184056+Dharshinipa@users.noreply.github.com>
2023-08-24 22:44:22 +0530

    Add files via upload

---
+++ array6.java
@@ -0,0 +1,10 @@
+public class ReverseArray {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+
+        System.out.println("Array elements in reverse order:");
+        for (int i = array.length - 1; i >= 0; i--) {
+            System.out.print(array[i] + " ");
+        }
+    }
+}
